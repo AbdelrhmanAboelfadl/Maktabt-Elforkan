51af368f56e4d2591917d73dd4d884450b67a5de
AbdelrhmanAboelfadl <bodefadl5972@gmail.com>
2025-10-17 21:41:16 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,32 @@
 $(document).ready(function() {
-
     $('#pagepiling').pagepiling({
-        direction: 'vertical', 
-        scrollingSpeed: 300,    
-        navigation: {           
-        'position': 'right',
-        'tooltips': ['Section 1', 'Section 2', 'Section 3']
-        }
-    });
+    direction: 'vertical',
+    navigation: false,
+    scrollingSpeed: 700,
+    anchors: ['section1', 'section2', 'section3'],
+    afterLoad: function(anchorLink, index) {
+        $(".navbar-nav li").removeClass("active");
 
+        if (anchorLink === 'section1') {
+        $(".navbar-nav li a[href='#section1']").parent().addClass("active");
+        } 
+        else if (anchorLink === 'section2') {
+        $(".navbar-nav li a[href='#section2']").parent().addClass("active");
+        } 
+        else if (anchorLink === 'section3') {
+        $(".navbar-nav li a[href='#section3']").parent().addClass("active");
+        }
+    }
+    });
+    $(".nav-link").click(function(e) {
+    e.preventDefault();
+    let target = $(this).attr("href").replace("#", "");
+    $.fn.pagepiling.moveTo(target);
+    });
 });
+
 $(".navbar-nav li").click(function () {
     addActiveToNavItem(this);
 });
+
+getData();
